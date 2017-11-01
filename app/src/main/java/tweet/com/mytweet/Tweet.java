3deb85bc74522978bc786a9d43774babab1387ba
Keelan Murphy <keelanmurphy90@gmail.com>
2017-11-01 11:43:02 +0000

    Created view for adding a tweet, added Tweet model, started move to MVC architecture

---
+++ app/src/main/java/tweet/com/mytweet/Tweet.java
@@ -0,0 +1,51 @@
+package tweet.com.mytweet;
+
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by keela on 01/11/2017.
+ */
+
+public class Tweet {
+    public Long id;
+    public Long date;
+    private String tweetMessage;
+
+    public Tweet()
+    {
+        id = unsignedLong();
+        date = new Date().getTime();
+    }
+
+    /**
+     * Generate a long greater than zero
+     * @return Unsigned Long value greater than zero
+     */
+    private Long unsignedLong() {
+        long rndVal = 0;
+        do {
+            rndVal = new Random().nextLong();
+        } while (rndVal <= 0);
+        return rndVal;
+    }
+
+    public void setTweetMessage(String tweetMessage)
+    {
+        this.tweetMessage = tweetMessage;
+    }
+
+    public String getTweetMessage()
+    {
+        return tweetMessage;
+    }
+
+    public String getDateString() {
+        return dateString();
+    }
+
+    private String dateString() {
+        String dateFormat = "EEE d MMM yyyy H:mm";
+        return android.text.format.DateFormat.format(dateFormat, date).toString();
+    }
+}
