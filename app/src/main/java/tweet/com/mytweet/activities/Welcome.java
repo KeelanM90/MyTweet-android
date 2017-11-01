52d0b6bc2a358e1341310215d55ca2725f493476
Keelan Murphy <keelanmurphy90@gmail.com>
2017-11-01 20:18:30 +0000

    Added welcome screen

---
+++ app/src/main/java/tweet/com/mytweet/activities/Welcome.java
@@ -0,0 +1,21 @@
+package tweet.com.mytweet.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import tweet.com.mytweet.R;
+
+/**
+ * Created by keela on 01/11/2017.
+ */
+
+public class Welcome extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
+
