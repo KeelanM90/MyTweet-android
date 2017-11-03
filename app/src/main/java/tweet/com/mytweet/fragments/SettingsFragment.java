d6fdfd1c17c243bb361d3ad637e5ebfb80ccc15d
Keelan Murphy <keelanmurphy90@gmail.com>
2017-11-03 22:28:59 +0000

    User can update their account settings

--- app/src/main/java/tweet/com/mytweet/fragments/SettingsFragment.java
+++ app/src/main/java/tweet/com/mytweet/fragments/SettingsFragment.java
@@ -6,18 +6,20 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
 import tweet.com.mytweet.R;
+import tweet.com.mytweet.app.MyTweetApp;
+import tweet.com.mytweet.models.User;
 
 /**
  * Created by keela on 02/11/2017.
  */
 
-public class SettingsFragment extends PreferenceFragment
-{
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private SharedPreferences prefs;
+    MyTweetApp app = MyTweetApp.getApp();
 
     @Override
-    public void onCreate(Bundle savedInstanceState)  {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
 
@@ -26,10 +28,25 @@ public class SettingsFragment extends PreferenceFragment
     }
 
     @Override
-    public void onStart()
-    {
+    public void onStart() {
         super.onStart();
-        prefs = PreferenceManager
-                .getDefaultSharedPreferences(getActivity());
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        prefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        User user = app.userStore.getUser(app.loggedInUser.id);
+        if (key.equals("email")) {
+            user.email = sharedPreferences.getString(key, "");
+        } else if (key.equals("password")) {
+            user.password = sharedPreferences.getString(key, "");
+        } else if (key.equals("firstname")) {
+            user.firstName = sharedPreferences.getString(key, "");
+        } else if (key.equals("lastname")) {
+            user.lastName = sharedPreferences.getString(key, "");
+        }
+        app.userStore.saveUsers();
     }
 }
